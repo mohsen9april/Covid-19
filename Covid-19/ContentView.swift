faf0b7537a5f8e7642c4041cf2561c7e70423538
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-06-04 22:18:48 +0430

    Initial Commit

---
+++ Covid-19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19
+//
+//  Created by Mohsen Abdollahi on 6/4/20.
+//  Copyright © 2020 Mohsen Abdollahi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
